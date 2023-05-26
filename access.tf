aab4ebf2d85d6dd9404fbd52c84b7ed8623c119b
Brad Sickles <brad.sickles@gmail.com>
2023-05-26 16:45:58 -0400

    Fixed effect on iam statement.

--- access.tf
+++ access.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "access" {
     for_each = local.kms_key_id == "" ? [] : [local.kms_key_id]
 
     content {
-      effect    = "AllowEncryptionKeyRead"
+      effect    = "Allow"
       resources = [statement.value]
       actions   = ["kms:Decrypt"]
     }
