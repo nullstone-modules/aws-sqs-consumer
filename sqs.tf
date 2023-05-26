2fdadf17141a21af92af1472dd1fae470eec7fa3
Brad Sickles <brad.sickles@gmail.com>
2023-05-26 08:03:51 -0400

    Added CMK decrypt access for encrypted sqs queues

--- sqs.tf
+++ sqs.tf
@@ -7,4 +7,5 @@ locals {
   queue_name = data.ns_connection.sqs.outputs.queue_name
   queue_url  = data.ns_connection.sqs.outputs.queue_url
   queue_arn  = data.ns_connection.sqs.outputs.queue_arn
+  kms_key_id = try(data.ns_connection.sqs.outputs.kms_key_id, "")
 }
